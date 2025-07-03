bf56f849888182e7438894af8f5b1f313a39533a
Shreyas G Salian <69745409+salian-shreyas@users.noreply.github.com>
2025-07-03 08:09:48 +0530

    Set-up express.js server
    Define necessary routes for the server

---
+++ index.js
@@ -0,0 +1,30 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const port = 3000;
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.sendStatus(200);
+});
+
+app.get("/create", (req, res) => {
+    res.sendStatus(200);
+});
+
+app.get("/view", (req, res) => {
+    res.sendStatus(200);
+});
+
+app.get("/edit", (req, res) => {
+    res.sendStatus(200);
+});
+
+app.get("/delete", (req, res) => {
+    res.sendStatus(200);
+});
+
+app.listen(port, (req, res) => {
+    console.log(`Server running on port: ${port}`);
+});
