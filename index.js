d306f09eeab7efb045befbe13b6b1e6c647a9f14
Shreyas G Salian <69745409+salian-shreyas@users.noreply.github.com>
2025-07-04 19:14:24 +0530

    Add feature to save blog post
    Save blog post to text file

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 import express from "express";
 import bodyParser from "body-parser";
-
-const port = 3000;
+import { writeFile } from "node:fs";
 
 const app = express();
+
+const port = 3000;
+const __dirname = import.meta.dirname;
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", (req, res) => {
     res.sendStatus(200);
@@ -14,7 +18,16 @@ app.get("/create", (req, res) => {
 });
 
 app.post("/create", (req, res) => {
-    res.sendStatus(200);
+    const fileName = blogFileName(req.body);
+    const filePath = fileNameToPath(fileName); 
+    const blog = blogText(req.body);
+
+    writeFile(filePath, blog, (err) => {
+        if (err) throw err;
+        console.log(`Blog saved to ${filePath}`);
+    });
+
+    res.redirect("/create");
 });
 
 app.get("/view", (req, res) => {
@@ -32,3 +45,15 @@ app.get("/delete", (req, res) => {
 app.listen(port, (req, res) => {
     console.log(`Server running on port: ${port}`);
 });
+
+function blogFileName(blog) {
+    return blog["blog-title"].split(" ").join("-") + ".txt";
+}
+
+function fileNameToPath(fileName) {
+    return __dirname + "/public/blogs/" + fileName;
+}
+
+function blogText(blog) {
+    return blog["blog-title"] + "\n\n" + blog["blog-body"];
+}
