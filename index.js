77b72becfcfdabe3ad6770b52857d6df1502b772
Shreyas G Salian <69745409+salian-shreyas@users.noreply.github.com>
2025-07-05 10:20:13 +0530

    Add home page
    Home page lists all the blogs with view, edit & delete buttons for each

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 import express from "express";
 import bodyParser from "body-parser";
 import { writeFile } from "node:fs";
+import { readdir } from "node:fs";
 
 const app = express();
 
 const port = 3000;
 const __dirname = import.meta.dirname;
+const blogFiles = [];
+
+loadBlogFiles();
 
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", (req, res) => {
-    res.sendStatus(200);
+    res.locals.blogs = blogTitles();
+    
+    res.render("home.ejs");
 });
 
 app.get("/create", (req, res) => {
@@ -19,13 +25,14 @@ app.get("/create", (req, res) => {
 
 app.post("/create", (req, res) => {
     const fileName = blogFileName(req.body);
-    const filePath = fileNameToPath(fileName); 
+    const filePath = blogFilePath(fileName); 
     const blog = blogText(req.body);
 
     writeFile(filePath, blog, (err) => {
         if (err) throw err;
         console.log(`Blog saved to ${filePath}`);
     });
+    blogFiles.push(fileName);
 
     res.redirect("/create");
 });
@@ -50,10 +57,28 @@ function blogFileName(blog) {
     return blog["blog-title"].split(" ").join("-") + ".txt";
 }
 
-function fileNameToPath(fileName) {
+function blogTitle(filename) {
+    return filename.split("-").join(" ").slice(0, -4);
+}
+
+function blogFilePath(fileName) {
     return __dirname + "/public/blogs/" + fileName;
 }
 
 function blogText(blog) {
     return blog["blog-title"] + "\n\n" + blog["blog-body"];
+}
+
+function blogTitles() {
+    return blogFiles.map(filename => blogTitle(filename) );
+}
+
+function loadBlogFiles() {
+    readdir(__dirname + "/public/blogs", (err, files) => {
+        if(err) throw err;
+
+        files.forEach(file => {
+            blogFiles.push(file);
+        });     
+    });
 }
